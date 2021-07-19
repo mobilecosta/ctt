741442a35fbaf40b4fbc036e04b5dad008673c39
luanpedro0014 <42987466+luanpedro0014@users.noreply.github.com>
2021-07-19 18:30:30 -0300

    Terceira Versão

---
+++ app/angular/src/app/modules/security/login/list.ts
@@ -0,0 +1,14 @@
+export interface List{
+
+  "ID": number,
+  "PDF_DTINI":Date,
+  "PDF_DTFIM":Date,
+  "PDM_CURSO":String,
+  "PD3_TURMA":Number,
+  "PD8_SALAS":String,
+  "PD2_PROFESSORES":String
+
+
+
+
+}
