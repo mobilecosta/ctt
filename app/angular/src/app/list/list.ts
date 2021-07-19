1664b5d7ae38b26a9d82b26b30cb9ea077ef38f4
luanpedro0014 <42987466+luanpedro0014@users.noreply.github.com>
2021-07-19 12:29:14 -0300

    Terceira Versão

--- app/angular/src/app/list/list.ts
+++ app/angular/src/app/list/list.ts
@@ -1,11 +1,12 @@
 export interface List{
 
-      "id":Number;
-      "Datas":Date;
-      "Curso":String;
-      "Turma":Number;
-      "Sala":String;
-      "Professor":String;
+  "ID": number,
+  "PDF_DTINI":Date,
+  "PDF_DTFIM":Date,
+  "PDM_CURSO":String,
+  "PD3_TURMA":Number,
+  "PD8_SALAS":String,
+  "PD2_PROFESSORES":String
 
 
 
