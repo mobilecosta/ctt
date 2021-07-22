0dcf61f8f2c6e8eabae585a456f6ebcd3da94a07
Wagner <mobile.costa@gmail.com>
2021-07-22 18:28:20 -0300

    Script de execução

---
+++ app/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 80
+
+const allowCors = (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // colocar os dominios permitidos | ex: 127.0.0.1:3000
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials, X-Access-Token, X-Key");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+}
+
+app.use(allowCors);
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+
+app.listen(port, async() => {
+	console.log(`API Gateway running in port ${port}!`)
+});
