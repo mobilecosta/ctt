6348c9df48b25e3de767d519a588b823700dec13
luanpedro0014 <42987466+luanpedro0014@users.noreply.github.com>
2021-08-14 11:42:54 -0300

    Estrutura de Login do Sistema

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PoModule } from '@po-ui/ng-components';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+      LoginComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PoModule,
+    RouterModule.forRoot([])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
