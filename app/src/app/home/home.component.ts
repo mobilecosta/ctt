3839cce8363f32369a2d78a9330a50f9eaed63c1
Wagner <mobile.costa@gmail.com>
2021-08-19 00:34:59 -0300

    Seleção dos cursos

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -13,6 +13,87 @@ export class HomeComponent {
 
   constructor(private router: Router, private storage: PoStorageService) { }
 
+  ngOnInit(): void {
+
+  }
+  
+  rowActions = {
+
+  };
+
+  columns = [
+    { property: 'datas', label: 'Datas', width:'50px',required:true},
+    { property: 'curso', label: 'Curso', align:'center', width: '100px' },
+    { property: 'turma', label: 'Turma', width: 80 },
+    { property: 'sala', label: 'Sala', width: 100,  },
+    { property: 'professor', label: 'Professor', width: 100, required: true },
+
+
+  ];
+
+  data = [
+    {
+      datas: '2021-08-04 até 2021-08-04',
+      curso: 'Configurador',
+      turma: '01',
+      sala:'A',
+      professor: 'João',
+
+    },
+    {
+      datas: '2021-08-08 até 2021-08-08',
+      curso: 'Módulo Financeiro',
+      turma: '01',
+      sala:'A',
+      professor: 'Fernanda',
+
+    },
+    {
+      datas: '2021-08-10 até 2021-08-10',
+      curso: 'Configurador',
+      turma: '02',
+      sala:'A',
+      professor: 'João',
+
+    },
+    {
+      datas: '2021-08-11 até 2021-08-11',
+      curso: 'Configurador',
+      turma: '03',
+      sala:'A',
+      professor: 'João',
+
+    },
+    {
+      datas: '2021-08-04 até 2021-08-04',
+      curso: 'Advpl Básico',
+      turma: '02',
+      sala:'B',
+      professor: 'Mário',
+
+    },
+    {
+      datas: '2021-08-05 até 2021-08-06',
+      curso: 'Módulo Faturamento',
+      turma: '01',
+      sala:'D',
+      professor: 'Jéssica',
+
+    },
+    {
+      datas: '2021-08-05 até 2021-08-08',
+      curso: 'Advpl Avançado',
+      turma: '01',
+      sala:'D',
+      professor: 'José',
+
+    }
+      ];
+
+  onBeforeSave(row: any, _old: any) {
+    return row.occupation !== 'Engineer';
+  }
+
   logout(): void {
   };
 
