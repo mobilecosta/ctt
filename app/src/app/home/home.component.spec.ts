e7e55d6442b903caf1ed77245051a2b70773845a
luanpedro0014 <luanpedro0014@gmail.com>
2021-08-23 22:43:04 -0300

    Atualizado.

--- app/src/app/home/home.component.spec.ts
+++ app/src/app/home/home.component.spec.ts
@@ -22,4 +22,4 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+})
--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { PoStorageService } from '@po-ui/ng-storage';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { PoDynamicViewField } from '@po-ui/ng-components';
+import { PoPageDynamicTableOptions } from '@po-ui/ng-templates';
 
 @Component({
   selector: 'app-home',
@@ -21,17 +23,16 @@ export class HomeComponent {
     this.httpClient.get('http://172.24.50.16:8044/CTT/api/login/26277712802') .subscribe (
 
       (datajson)=>{
-        var curso = datajson.aCursos
+     var curso = datajson.aCursos         //Variavel criado a partir do Json.
+      datajson.aCursos.forEach((value,index) => {            //Foreach para percorrer o caminho do Json a cursos.
 
-        datajson.aCursos.forEach((value,index) => {
 
-
-          console.log(datajson.aCursos);
-          this.data.push(
+         // console.log(datajson.aCursos);
+          this.data.push(                        // push para adicionar no front end os dados da Api Json.
             {
 
-            datas: `${value['PDF_DTINI']}  até ${value['PDF_DTFIM']}`,
-            curso: value['PD3_NOME'],
+            datas: `${value['PDF_DTINI']}  até ${value['PDF_DTFIM']}`,   // Concatenação dos dados da APi 'PDF_Ini até 'PDF_Ini'
+            curso: value['PD3_NOME'],    // PD3_Nome (nome do campo API.)
             turma: value['PD7_TURMA'],
             sala: value['PD3_SALA'],
             professor:value['PD2_NOME']
@@ -41,11 +42,6 @@ export class HomeComponent {
 
         });
 
-        console.log(this.data);
-      },
-      (error)=>{
-
-        console.log(error);
 
 
       }
@@ -62,11 +58,19 @@ export class HomeComponent {
   };
 
   columns = [
+
+
+
+    { property: 'status', tag: true, gridLgColumns: 4, divider: 'Personal Data', booleanTrue:"" ,checkbox:""},
+
+
+
+
     { property: 'datas', label: 'Datas', width:'50px',required:true},
     { property: 'curso', label: 'Curso', align:'center', width: '100px' },
-    { property: 'turma', label: 'Turma', width: 80 },
-    { property: 'sala', label: 'Sala', width: 100,  },
-    { property: 'professor', label: 'Professor', width: 100, required: true }
+    { property: 'turma', label: 'Turma', width: 150 },
+    { property: 'sala', label: 'Sala', width: 150,  },
+    { property: 'professor', label: 'Professor', width: 400, required: true }
   ];
 
   data = [
@@ -75,6 +79,15 @@ export class HomeComponent {
   onBeforeSave(row: any, _old: any) {
     return row.occupation !== 'Engineer';
   }
+
+
+
+    onClick() {
+     // alert('input[name=radioName]:checked');
+     window.location.href = "http://localhost:4200/perguntas"
+
+    }
+
 
   logout(): void {
   };
