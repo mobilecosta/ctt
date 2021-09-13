fd94204a7477675104a1ae7c91062fec702c2bd3
Wagner <mobile.costa@gmail.com>
2021-09-13 10:23:51 -0300

    Ajuste da API

--- app/src/environments/environment.pre.ts
+++ app/src/environments/environment.pre.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   wso2: true,
   urltoken: 'https://apimqa.totvs.com.br/api-homologacao/token',
-  api: 'http://apimprod.totvs.com.br/api/protheus-ctt-portal/v1.0/'
+  api: 'https://apimqa.totvs.com.br/api-homologacao/protheus-ctt-portal/v1.0/'
 };
