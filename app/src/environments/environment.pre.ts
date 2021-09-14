4d90d471d5d6f91815704e1dfa8df8fba0a46fcf
Wagner <mobile.costa@gmail.com>
2021-09-14 14:13:11 -0300

    Atualização

--- app/src/environments/environment.pre.ts
+++ app/src/environments/environment.pre.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   wso2: true,
   urltoken: 'https://apimqa.totvs.com.br/api-homologacao/token',
   api: 'https://apimqa.totvs.com.br/api-homologacao/protheus-ctt-portal/v1.0/'
