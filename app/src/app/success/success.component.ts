87cd0930a901a44c9ac2f2e85566e638386a4611
Wagner <mobile.costa@gmail.com>
2021-09-14 13:43:42 -0300

    Recarga dos dados após gravação

--- app/src/app/success/success.component.ts
+++ app/src/app/success/success.component.ts
@@ -3,6 +3,7 @@ import { PoDynamicViewField, PoListViewLiterals } from '@po-ui/ng-components';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { PoStorageService } from '@po-ui/ng-storage';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './success.component.html',
@@ -20,7 +21,10 @@ export class SuccessComponent implements OnInit {
   ]
 
   employee = {}
-  constructor(private storage: PoStorageService) {
+  cpf: string;
+  constructor(private router: Router, 
+              private httpClient: HttpClient,
+              private storage: PoStorageService) {
 
     this.storage.get('user').then((res)=>{
        this.employee = {
@@ -29,12 +33,35 @@ export class SuccessComponent implements OnInit {
          business: `${res.A1_CGC} - ${res.A1_NOME}`,
          class: `${res.PDL_NOME}`
        }
+       this.cpf = res.PDL_CPF;
      })
 
   }
 
   ngOnInit() {
   }
+
+  onClick() {
+    var url_login = environment.api + "api/login/" + this.cpf;
+    // Autenticação Metodo LOGIN baseado no CPF
+    this.httpClient.get(url_login).subscribe((res) => {
+      if( res["resultado"] ==  1){
+        this.storage.set('user', res).then(()=>{
+          this.router.navigate(['/']);
+        })
+      } else{
+        localStorage.setItem('access_token', " ");
+        window.alert(res["DESCRICAO"])
+      }
+
+    }, (error) =>{
+      if(error.hasOwnProperty('message')){
+        window.alert(error.message);
+      }
+
+    })
+         
+  };
 
 }
 
