58dcacb9bbcd502593c947b5b1d6537d173e2343
Wagner <mobile.costa@gmail.com>
2022-03-28 00:01:52 -0300

    Teste

--- app/src/environments/environment.pre.ts
+++ app/src/environments/environment.pre.ts
@@ -5,6 +5,7 @@ export const environment = {
   api: 'https://apimqa.totvs.com.br/api-homologacao/protheus-ctt-portal/v1.0/',
   token: 'UjJhbHJFZDRoQWh1MmZSMFRPQnVCTlpxdFM0YTpsUDBUYktKUDdmQ245WGJDUktkM2pYZDFYRW9h'
 //  wso2: false,
+//  urltoken: 'https://api.conceitho.com/api/protheus/api/oauth2/v1/token?grant_type=password&password=admin&username=admin',
 //  urltoken: 'https://prewscorp.totvs.com.br/api/oauth2/v1/token?grant_type=password&password=303351@wag_2a&username=WAGNER.COSTA_UTPT',
 //  api: 'https://prewscorp.totvs.com.br/ctt/',
 //  token: ''
